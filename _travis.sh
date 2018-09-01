53f3eeaa2b4b5a4942a45b3357577c7dac8f5a21
polymonster <alexandercdixon@gmail.com>
2018-09-01 18:48:38 +0100

    setup sdk and ndk for travis

--- _travis.sh
+++ _travis.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+# java9 causes an exception with android sdk manager
+brew cask uninstall java
+brew tap caskroom/versions
+brew cask install java8
+touch ~/.android/repositories.cfg
+brew cask install android-sdk
+
 export ANDROID_HOME="/usr/local/share/android-sdk"
 export ANDROID_NDK_HOME="/usr/local/share/android-ndk"
 yes | sudo sdkmanager --licenses
