2ddb8ca7061b48c80fe0562e6089179f45b8ecfe
polymonster <alexandercdixon@gmail.com>
2018-09-01 18:17:40 +0100

    setup sdk and ndk for travis

--- _travis.sh
+++ _travis.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 export ANDROID_HOME="/usr/local/share/android-sdk"
 export ANDROID_NDK_HOME="/usr/local/share/android-ndk"
+yes | sudo sdkmanager --licenses
 cd example
 ../premake5 android-studio
 cd build
