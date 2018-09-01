dc3874b153a4c47faad4b5d5beedb876c727bba4
polymonster <alexandercdixon@gmail.com>
2018-09-01 18:58:23 +0100

    setup sdk and ndk for travis

--- _travis.sh
+++ _travis.sh
@@ -5,12 +5,14 @@ brew tap caskroom/versions
 brew cask install java8
 touch ~/.android/repositories.cfg
 brew cask install android-sdk
+brew cask install android-ndk
 
 export ANDROID_HOME="/usr/local/share/android-sdk"
 export ANDROID_NDK_HOME="/usr/local/share/android-ndk"
 yes | sudo sdkmanager --licenses
 yes | $ANDROID_HOME/tools/bin/sdkmanager "platforms;android-25"
 yes | $ANDROID_HOME/tools/bin/sdkmanager "build-tools;27.0.3"
+
 cd example
 ../premake5 android-studio
 cd build
