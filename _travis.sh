709b7388104c62f3c850e60097cf990b35b40fd2
Alex Dixon <alexandercdixon@gmail.com>
2019-07-09 20:14:44 +0100

    Update _travis.sh

--- _travis.sh
+++ _travis.sh
@@ -1,18 +1,4 @@
 #!/usr/bin/env bash
-# java9 causes an exception with android sdk manager
-brew cask uninstall java
-brew tap caskroom/versions
-brew cask install java8
-touch ~/.android/repositories.cfg
-brew cask install android-sdk
-brew cask install android-ndk --no-quarantine
-
-export ANDROID_HOME="/usr/local/share/android-sdk"
-export ANDROID_NDK_HOME="/usr/local/share/android-ndk"
-yes | sudo sdkmanager --licenses
-yes | $ANDROID_HOME/tools/bin/sdkmanager "platforms;android-25"
-yes | $ANDROID_HOME/tools/bin/sdkmanager "build-tools;27.0.3"
-
 cd example
 ../premake5 android-studio
 cd build
