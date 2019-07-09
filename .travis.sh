a8ec97639565f86daf9a628fbb3b14499a653fe8
Alex Dixon <alexandercdixon@gmail.com>
2019-07-09 22:06:20 +0100

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 brew cask install android-sdk
-export JAVA_OPTS='-XX:+IgnoreUnrecognizedVMOptions --add-modules java.se.ee'
-yes | sudo sdkmanager --licenses
+yes | sudo sdkmanager --add-modules java.xml.bind --licenses
 
