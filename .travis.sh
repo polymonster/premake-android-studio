689cc41ec28df999f336156d8a39abb0c3ae72a0
Alex Dixon <alexandercdixon@gmail.com>
2019-07-18 00:32:31 +0100

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -3,6 +3,8 @@
 # this script no longer works on ci because android sdk manager crashes with java9+ and java8 can no longer be installed via home brew.
 # to succesfully install android sdk and ndk get android studio and it will install them for you, you can then use the commandline.
 
-cd example/build
+cd example
+../premake5 android-studio
+cd build
 gradle build --stacktrace
 
